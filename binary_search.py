3b9ba8398024b8a91962a8ef4a76392fab4ebf11
osame001 <osame001@ucr.edu>
2022-01-10 15:39:11 +0330

    Update binary_search.py
    added some description.

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,8 @@
+'''
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+'''
 import random
 class Binary_search:
     def search(self, nums , target):
