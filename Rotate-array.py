64bbbb8a8019e4f0eccb24556cf69f620508573f
osame001 <osame001@ucr.edu>
2022-01-18 11:52:00 -0800

    Create Rotate-array.py

---
+++ Rotate-array.py
@@ -0,0 +1,21 @@
+'''
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+'''
+class Solution:
+    def rotate(self, nums, k):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k%len(nums)
+        l , r = 0, len(nums)-1
+        while l<r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+        l , r = 0, k-1
+        while l<r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
+        l , r = k, len(nums)-1
+        while l<r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l+1, r-1
